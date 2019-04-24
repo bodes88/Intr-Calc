826d27ab4841fb77599e558d22d5f75e765b0818
Bodie <bodie@bodie.com>
2019-04-24 14:43:07 +1000

    first commit

---
+++ javascript.js
@@ -0,0 +1,15 @@
+﻿function calculate() {
+    var x, text;
+    x = document.getElementById("numb").value;
+
+    if (isNaN(x) || x < 0 || x > 30) {
+        text = 'Please enter a valid interest rate';
+    } else {
+        text = 'Valid interest rate';
+    }
+    document.getElementById("test").innerHTML = text;
+}
+
+function button() {
+<button type="button" onclick="calculate()">Calculate</button>
+    }
---
+++ main.js
@@ -0,0 +1,23 @@
+﻿
+function calc() {
+    var a = parseInt(document.querySelector("#value1").value);
+    var b = parseInt(document.querySelector("#value2").value);
+    var mySaver = 2.6
+    var bonusSaver = 2.55 + 0.15
+    var pensionSaver = 2.2
+    var childSaver = 3.9
+    var op = document.querySelector("#operator").value;
+    var calculate;
+
+    if (op == "add") {
+    calculate = a + b;
+    } else if (op == "min") {
+    calculate = a - b;
+    } else if (op == "div") {
+    calculate = a / b;
+    } else if (op == "mul") {
+    calculate = a * b;
+    }
+
+    document.querySelector("#result").innerHTML = calculate;
+}
